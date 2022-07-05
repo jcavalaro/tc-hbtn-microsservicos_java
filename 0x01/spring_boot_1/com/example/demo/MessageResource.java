b97ff9513808a677db0de4d6eec9cfbe4e219a88
jcavalaro <jcavalaro@ciandt.com>
2022-07-05 13:40:32 -0300

    sprint boot

---
+++ 0x01/spring_boot_1/com/example/demo/MessageResource.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/messages")
+public class MessageResource {
+
+    @GetMapping("/simpleMessageWelcome")
+    public String simpleMessageWelcome() {
+
+        return "BEM VINDO A AULA DE MICROSSERVIÇO USANDO SPRING BOOT !!!";
+
+    }
+
+    @GetMapping("/login")
+    public String login(@RequestParam String user,@RequestParam String password) {
+
+        if (user.isEmpty() || user.isBlank() || password.isBlank() || password.isEmpty()) {
+            return "USUÁRIO E SENHA NÃO INFORMADOS";
+        } else if (user.length() <= 15 || password.length() <= 15) {
+            return "USUÁRIO E SENHA INVÁLIDOS";
+        }
+        return "LOGIN EFETUADO COM SUCESSO !!!";
+    }
+}
